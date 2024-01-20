eea4e9b0021819f71dbda55fe7aef0355d5c2320
velan49 <157209350+velan49@users.noreply.github.com>
2024-01-20 15:31:17 +0530

    Add files via upload

---
+++ collection.py
@@ -0,0 +1,27 @@
+collection=['msd',7,'vijay']
+for x in collection:
+    print(x)
+
+brand=["thala","king","hitman"]
+player=["dhoni","virat","rohit"]
+for x in brand:
+    for y in player:
+        print(x,y)
+    
+lists=[['v,e,l,a,n'],['s,a,n,j,a,y'],['m,u,k,e,s,h']]
+for v in lists:
+    print(v)
+    for x in v:
+        print(x)
+
+fruits=["apple","banana","cherry"]
+for x in fruits:
+    if x=="apple":
+        continue
+    print(x)
+
+for a in range(50,80,4):
+    print(a)
+
+for x in range(7):
+    pass
